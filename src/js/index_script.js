1c3d51d8ffe8f7c3d32461d278ac31843ad6064b
Paul-Marie-beep <paul-marie.debrie@hotmail.fr>
2022-11-05 15:48:12 +0100

    nouveau test

--- src/js/index_script.js
+++ src/js/index_script.js
@@ -34,7 +34,9 @@ const loadingGif = document.querySelector(".loading__gif");
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // Test à effacer
 
-document.querySelector(".title-text--h1").innerHTML = `Taille de l'écran : ${window.innerWidth}`;
+document.querySelector(
+  ".title-text--h1"
+).innerHTML = `Taille de l'écran : ${window.innerWidth} Taille de l'écran : ${window.screen.availWidth}`;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // Loader
